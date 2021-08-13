58dbae874d984e05542f530f83786f7aef64ed42
João Vasconcelos <jvsn19@gmail.com>
2021-08-12 22:19:42 -0300

    update main to read files
    now the main should read files instead of passing as executable input

--- gss/main.cpp
+++ gss/main.cpp
@@ -3,11 +3,12 @@
 #define GSS_F 100
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <chrono>
 
 #include "gss.cpp"
 
-using namespace std;
 
 // Pass those values as arguments
 int limit = 103;
@@ -48,11 +49,14 @@ int hashFunction(int v) {
 
 void run(string filePath) {
     GSS<int>* gss = new GSS<int>(GSS_M, GSS_m, GSS_F, hashFunction);
+    std::ifstream file(filePath);
+    int origin, destiny;
+    string line;
     auto start = chrono::high_resolution_clock::now();
-    int a, b;
-    while (!cin.eof()) {
-        cin >> a >> b;
-        gss->insertEdge(make_tuple(make_pair(a,b), 1));
+    while (getline(file, line)) {
+        istringstream ss(line);
+        ss >> origin >> destiny;
+        gss->insertEdge(make_tuple(make_pair(origin, destiny), 1));
     }
     auto stop = chrono::high_resolution_clock::now();
     cout << "Duration: " << chrono::duration_cast<chrono::microseconds>(stop - start).count() << endl;
